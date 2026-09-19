c5da1f247f7babbb7595f73e20e88cc63c51dbec
Gr3yPh <gr3yph4ntom@outlook.jp>
2026-09-19 19:00:47 +0800

    style: 将广告轮播位默认图像切换为空白

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -19,7 +19,7 @@ type CarouselAd = {
   link: string
 }
 
-const DEFAULT_ADS: CarouselAd[] = [{ image: '/', link: '/' }]
+const DEFAULT_ADS: CarouselAd[] = [{ image: '/white.png', link: '/' }]
 
 const adItems = ref<CarouselAd[]>([...DEFAULT_ADS])
 const currentAdIndex = ref(0)
